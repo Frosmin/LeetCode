4c1da937a52e31268fc19529388f41cf0644a4ed
Frosmin <simonabastomart@gmail.com>
2024-05-24 23:22:33 -0400

    tercer problema

---
+++ ExprecionesRegulares.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+
+        dp[-1][-1] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j+1 < len(p) and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
+
+        return dp[0][0]
